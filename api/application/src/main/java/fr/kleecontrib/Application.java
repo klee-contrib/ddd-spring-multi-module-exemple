c0b4ede496abda79436bdacca0b8aaa77d8d4b90
Thomas Cresson <58069674+Thomas-cresson-pro@users.noreply.github.com>
2022-11-24 15:08:13 +0100

    ptit docker compose + maj spring framework

--- api/application/src/main/java/fr/kleecontrib/Application.java
+++ api/application/src/main/java/fr/kleecontrib/Application.java
@@ -1,15 +1,11 @@
 package fr.kleecontrib;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = { "fr.kleecontrib" })
-@EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"fr.kleecontrib"})
 public class Application extends SpringBootServletInitializer {
 
 	@SuppressWarnings("squid:S4823")
